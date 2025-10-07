fc6ea1516696eaea2e4b8a38f4a03f31793a63c1
Giridharanp1 <giridharan.p2024aids@sece.ac.in>
2025-10-07 20:03:10 +0530

    Update main.tsx

--- studybuddy-moodflow-main/src/main.tsx
+++ studybuddy-moodflow-main/src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
+import './studybuddy-moodflow-main/index.css';
 
 createRoot(document.getElementById("root")!).render(<App />);
